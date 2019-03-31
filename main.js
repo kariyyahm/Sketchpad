d95749cf10b23a646ad8ad78af609ce0d06bea3a
kari <mr_hm@outlook.com>
2019-03-31 23:34:48 +0800

    支持触屏设备

--- main.js
+++ main.js
@@ -10,26 +10,40 @@ var context = yyy.getContext('2d') //声明上下文
 
 var using = false //声明一个画圆的状态
 
+var useEraser = false //声明橡皮的状态
+eraser.onclick = function () {
+    useEraser = !useEraser
+}
+
 var lastPoint = { x: undefined, y: undefined } //声明一个点
+
 yyy.onmousedown = function (aaa) {
     using = true
     var x = aaa.clientX
     var y = aaa.clientY
-    lastPoint = { x: x, y: y }
-    drawCircle(x, y, 1)
+    if (using) {
+        if (!useEraser) {
+            lastPoint = { x: x, y: y }
+            drawCircle(x, y, 1)
+        } else { context.clearRect(x - 10, y - 10, 20, 20) }
+    }
 }
 yyy.onmousemove = function (aaa) {
+    var x = aaa.clientX
+    var y = aaa.clientY
     if (using) {
-        var x = aaa.clientX
-        var y = aaa.clientY
-        var newPoint = { x: x, y: y }
-        drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
-        lastPoint = newPoint
-    } else { }
+        if (!useEraser) {
+            var newPoint = { x: x, y: y }
+            drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
+            lastPoint = newPoint
+        } else { context.clearRect(x - 10, y - 10, 20, 20) }
+    }
 }
+
 yyy.onmouseup = function (aaa) {
     using = false
 }
+
 function drawCircle(x, y, radius) {
     context.beginPath()
     context.arc(x, y, radius, 0, Math.PI * 2)
