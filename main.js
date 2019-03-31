f4fa843f962ecdec278a44125d6bb6325c1ef76d
kari <mr_hm@outlook.com>
2019-04-01 01:21:29 +0800

    实现功能

--- main.js
+++ main.js
@@ -2,9 +2,6 @@ var yyy = document.getElementById('xxx') //从html获取canvas
 
 windowSize()
 
-window.onresize = function () {
-    windowSize()
-}
 
 var context = yyy.getContext('2d') //声明上下文
 
@@ -12,36 +9,122 @@ var using = false //声明一个画圆的状态
 
 var useEraser = false //声明橡皮的状态
 eraser.onclick = function () {
-    useEraser = !useEraser
+    useEraser = true
+}
+pencil.onclick = function () {
+    useEraser = false
+}
+thickness.onclick = function () {
+
+}
+blackPencil.onclick = function () {
+    useEraser = false
+    context.fillStyle = "black"
+    context.strokeStyle = "black"
+}
+grayPencil.onclick = function () {
+    useEraser = false
+    context.fillStyle = "#d8d8d8"
+    context.strokeStyle = "#d8d8d8"
+}
+bluePencil.onclick = function () {
+    useEraser = false
+    context.fillStyle = "#208BFF"
+    context.strokeStyle = "#208BFF"
+}
+redPencil.onclick = function () {
+    useEraser = false
+    context.fillStyle = "#FF2042"
+    context.strokeStyle = "#FF2042"
+}
+greenPencil.onclick = function () {
+    useEraser = false
+    context.fillStyle = "#2FD865"
+    context.strokeStyle = "#2FD865"
+}
+yellowPencil.onclick = function () {
+    useEraser = false
+    context.fillStyle = "#FFDC36"
+    context.strokeStyle = "#FFDC36"
+}
+window.onresize = function () {
+    windowSize()
+}
+clear.onclick = function() {
+    context.clearRect(0, 0, yyy.width, yyy.height)
+}
+undo.onclick = function () {
+
+}
+repeat.onclick = function () {
+
+}
+save.onclick = function () {
+    var url = yyy.toDataURL('image/png')
+    console.log(url)
+    var a = document.createElement('a')
+    document.body.appendChild(a)
+    a.href = url
+    a.download = '柠檬画板'
+    a.click()
 }
 
 var lastPoint = { x: undefined, y: undefined } //声明一个点
 
-yyy.onmousedown = function (aaa) {
-    using = true
-    var x = aaa.clientX
-    var y = aaa.clientY
-    if (using) {
-        if (!useEraser) {
-            lastPoint = { x: x, y: y }
-            drawCircle(x, y, 1)
-        } else { context.clearRect(x - 10, y - 10, 20, 20) }
+if (document.body.ontouchstart !== undefined) {
+    yyy.ontouchstart = function (aaa) {
+        using = true
+        var x = aaa.touches[0].clientX
+        var y = aaa.touches[0].clientY
+        if (using) {
+            if (!useEraser) {
+                lastPoint = { x: x, y: y }
+                drawCircle(x, y, 1)
+            } else { context.clearRect(x - 10, y - 10, 20, 20) }
+        }
     }
-}
-yyy.onmousemove = function (aaa) {
-    var x = aaa.clientX
-    var y = aaa.clientY
-    if (using) {
-        if (!useEraser) {
-            var newPoint = { x: x, y: y }
-            drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
-            lastPoint = newPoint
-        } else { context.clearRect(x - 10, y - 10, 20, 20) }
+    yyy.ontouchmove = function (aaa) {
+        var x = aaa.touches[0].clientX
+        var y = aaa.touches[0].clientY
+        if (using) {
+            if (!useEraser) {
+                var newPoint = { x: x, y: y }
+                drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
+                lastPoint = newPoint
+            } else { context.clearRect(x - 10, y - 10, 20, 20) }
+        }
     }
-}
 
-yyy.onmouseup = function (aaa) {
-    using = false
+    yyy.ontouchend = function (aaa) {
+        using = false
+    }
+} else {
+    yyy.onmousedown = function (aaa) {
+        using = true
+        var x = aaa.clientX
+        var y = aaa.clientY
+        if (using) {
+            if (!useEraser) {
+                lastPoint = { x: x, y: y }
+                drawCircle(x, y, 1)
+            } else { context.clearRect(x - 10, y - 10, 20, 20) }
+        }
+    }
+    yyy.onmousemove = function (aaa) {
+        var x = aaa.clientX
+        var y = aaa.clientY
+        if (using) {
+            if (!useEraser) {
+                var newPoint = { x: x, y: y }
+                drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
+                lastPoint = newPoint
+            } else { context.clearRect(x - 10, y - 10, 20, 20) }
+        }
+    }
+
+    yyy.onmouseup = function (aaa) {
+        using = false
+    }
 }
 
 function drawCircle(x, y, radius) {
